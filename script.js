b685cc584fb163a55c022d4aebd661fa1889b700
amitnandagit <amit.nanda@equitysoft.in>
2025-11-13 11:14:36 +0530

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -202,5 +202,34 @@ document.addEventListener('DOMContentLoaded', () => {
             setInterval(updateHeroSlider, slideInterval);
         }
     }
+    // Initialize Swiper for testimonials
+        const testimonialSwiper = new Swiper('.testimonial-swiper', {
+            slidesPerView: 1,
+            spaceBetween: 30,
+            loop: true,
+            autoplay: {
+                delay: 5000,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            breakpoints: {
+                768: {
+                    slidesPerView: 1,
+                    spaceBetween: 40,
+                },
+                1024: {
+                    slidesPerView: 1,
+                    spaceBetween: 50,
+                }
+            }
+        });
+
     
 });
